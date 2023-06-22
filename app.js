ad1e32048d5cf0ec8cbc78107d7db426d9717e95
NRJ YDV <88180887+NeeRaj-2401@users.noreply.github.com>
2023-06-22 13:11:33 +0530

    mern banking

---
+++ app.js
@@ -0,0 +1,50 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import { users } from "./defaultUser.js";
+import User from "./models/user.js";
+
+import userRoutes from "./routes/users.js"; // imp
+import transactionsRoutes from "./routes/transactions.js"; // imp
+
+const PORT =  process.env.PORT || 4040 ;
+
+const app = express();
+dotenv.config();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/users", userRoutes); // imp /users/register
+app.use("/transactions", transactionsRoutes); // imp /users/register
+
+const createDefaultUsers = async () => {
+  try {
+    for (const userData of users) {
+      const { username } = userData;
+      const existingUser = await User.findOne({ username });
+      console.log(existingUser);
+      if (!existingUser) {
+        await User.create(userData);
+        console.log(`User created: ${username}`);
+      }
+    }
+  } catch (error) {
+    console.error("Error creating default users:", error);
+  }
+}
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, async () => {
+      await createDefaultUsers();
+      console.log(`Server Running on Port: http://localhost:${PORT}`);
+    })
+  )
+  .catch((e) => console.log(e.message));
